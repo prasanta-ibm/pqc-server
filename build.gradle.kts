7f2fb87b9872588376b6e86cdd858a9eca3ac6fc
Prasanta Kumar Sahoo <prasantakumar.sahoo@ibm.com>
2026-07-03 20:41:50 +0530

    Main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,12 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:2.0.18")
+    implementation("org.slf4j:slf4j-simple:2.0.18")
+}
+
+application {
+    mainClass.set("com.terracottatech.TlsApp")
+}
---
+++ src/main/java/com/terracottatech/TlsApp.java
@@ -0,0 +1,12 @@
+package com.terracottatech;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TlsApp {
+  private static final Logger logger = LoggerFactory.getLogger(TlsApp.class);
+
+  public static void main(String[] args) {
+    logger.info("Starting the PQC Server App");
+  }
+}
